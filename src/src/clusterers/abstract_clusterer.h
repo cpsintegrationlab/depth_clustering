b3dcc3672a785190bf362b658e533eab19b5a8e4
Simon Yu <jundayu2@illinois.edu>
2020-03-10 18:47:08 -0500

    Formatted abstract clusterer class.

--- src/src/clusterers/abstract_clusterer.h
+++ src/src/clusterers/abstract_clusterer.h
@@ -23,45 +23,45 @@
 #include "communication/abstract_sender.h"
 #include "utils/cloud.h"
 
-namespace depth_clustering {
+namespace depth_clustering
+{
 
 /**
  * @brief      Class for abstract clusterer.
  */
-class AbstractClusterer
-    : public AbstractClient<Cloud>,
-      public AbstractSender<std::unordered_map<uint16_t, Cloud>> {
- public:
-  using Receiver = AbstractClient<Cloud>;
-  using Sender = AbstractSender<std::unordered_map<uint16_t, Cloud>>;
+class AbstractClusterer: public AbstractClient<Cloud>, public AbstractSender<
+		std::unordered_map<uint16_t, Cloud>>
+{
+public:
+	using Receiver = AbstractClient<Cloud>;
+	using Sender = AbstractSender<std::unordered_map<uint16_t, Cloud>>;
 
-  /**
-   * @brief      Construct a clusterer.
-   *
-   * @param[in]  cluster_tollerance  The cluster tollerance
-   * @param[in]  min_cluster_size    The minimum cluster size
-   * @param[in]  max_cluster_size    The maximum cluster size
-   * @param[in]  skip                Only cluster every skip cloud
-   */
-  explicit AbstractClusterer(double cluster_tollerance = 0.2,
-                             uint16_t min_cluster_size = 100,
-                             uint16_t max_cluster_size = 25000,
-                             uint16_t skip = 10)
-      : Receiver(),
-        Sender(SenderType::STREAMER),
-        _cluster_tollerance(cluster_tollerance),
-        _min_cluster_size(min_cluster_size),
-        _max_cluster_size(max_cluster_size),
-        _skip(skip),
-        _counter(0) {}
-  virtual ~AbstractClusterer() {}
+	/**
+	 * @brief      Construct a clusterer.
+	 *
+	 * @param[in]  cluster_tollerance  The cluster tollerance
+	 * @param[in]  min_cluster_size    The minimum cluster size
+	 * @param[in]  max_cluster_size    The maximum cluster size
+	 * @param[in]  skip                Only cluster every skip cloud
+	 */
+	explicit
+	AbstractClusterer(double cluster_tollerance = 0.2, uint16_t min_cluster_size = 100,
+			uint16_t max_cluster_size = 25000, uint16_t skip = 10) :
+			Receiver(), Sender(SenderType::STREAMER), _cluster_tollerance(cluster_tollerance), _min_cluster_size(
+					min_cluster_size), _max_cluster_size(max_cluster_size), _skip(skip), _counter(0)
+	{
+	}
+	virtual
+	~AbstractClusterer()
+	{
+	}
 
- protected:
-  double _cluster_tollerance;
-  uint16_t _min_cluster_size;
-  uint16_t _max_cluster_size;
-  uint16_t _skip;
-  uint32_t _counter;
+protected:
+	double _cluster_tollerance;
+	uint16_t _min_cluster_size;
+	uint16_t _max_cluster_size;
+	uint16_t _skip;
+	uint32_t _counter;
 };
 
 }  // namespace depth_clustering
