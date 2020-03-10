68966b9a3d8b78c687be0fbee00f05de8eda1a34
Simon Yu <jundayu2@illinois.edu>
2020-03-10 18:21:29 -0500

    Formatted depth ground remover class.

--- src/src/ground_removal/depth_ground_remover.h
+++ src/src/ground_removal/depth_ground_remover.h
@@ -26,7 +26,8 @@
 #include "utils/radians.h"
 #include "utils/cloud.h"
 
-namespace depth_clustering {
+namespace depth_clustering
+{
 
 /**
  * @brief      A class to remove ground based upon depth image
@@ -36,108 +37,121 @@ namespace depth_clustering {
  *
  * @param      params  projection params
  */
-class DepthGroundRemover : public AbstractClient<Cloud>,
-                           public AbstractSender<Cloud> {
-  using ClientT = AbstractClient<Cloud>;
-  using SenderT = AbstractSender<Cloud>;
+class DepthGroundRemover: public AbstractClient<Cloud>, public AbstractSender<Cloud>
+{
+	using ClientT = AbstractClient<Cloud>;
+	using SenderT = AbstractSender<Cloud>;
 
- public:
-  explicit DepthGroundRemover(const ProjectionParams& params,
-                              const Radians& ground_remove_angle,
-                              int window_size = 5)
-      : ClientT{},
-        SenderT{SenderType::STREAMER},
-        _params{params},
-        _window_size{window_size},
-        _ground_remove_angle{ground_remove_angle} {}
-  virtual ~DepthGroundRemover() {}
+public:
+	explicit
+	DepthGroundRemover(const ProjectionParams &params, const Radians &ground_remove_angle,
+			int window_size = 5) :
+			ClientT
+			{ }, SenderT
+			{ SenderType::STREAMER }, _params
+			{ params }, _window_size
+			{ window_size }, _ground_remove_angle
+			{ ground_remove_angle }
+	{
+	}
+	virtual
+	~DepthGroundRemover()
+	{
+	}
 
-  /**
-   * @brief      when someone sends us an object we process it
-   * @details    receiving a depth image we remove ground from it and send to
-   *             next recepient
-   *
-   * @param      depth_image  32 bit depth image
-   * @param      sender_id    id of the sender
-   */
-  void OnNewObjectReceived(const Cloud& cloud, const int sender_id) override;
+	/**
+	 * @brief      when someone sends us an object we process it
+	 * @details    receiving a depth image we remove ground from it and send to
+	 *             next recepient
+	 *
+	 * @param      depth_image  32 bit depth image
+	 * @param      sender_id    id of the sender
+	 */
+	void
+	OnNewObjectReceived(const Cloud &cloud, const int sender_id) override;
 
- protected:
-  /**
-   * @brief      Zero out all pixels that belong to ground
-   *
-   * @param[in]  image        Input depth image
-   * @param[in]  angle_image  The angle image
-   * @param[in]  threshold    angle threshold
-   *
-   * @return     depth image with 0 instead of ground pixels
-   */
-  cv::Mat ZeroOutGround(const cv::Mat& image, const cv::Mat& angle_image,
-                        const Radians& threshold) const;
+protected:
+	/**
+	 * @brief      Zero out all pixels that belong to ground
+	 *
+	 * @param[in]  image        Input depth image
+	 * @param[in]  angle_image  The angle image
+	 * @param[in]  threshold    angle threshold
+	 *
+	 * @return     depth image with 0 instead of ground pixels
+	 */
+	cv::Mat
+	ZeroOutGround(const cv::Mat &image, const cv::Mat &angle_image, const Radians &threshold) const;
 
-  cv::Mat ZeroOutGroundBFS(const cv::Mat& image, const cv::Mat& angle_image,
-                           const Radians& threshold, int kernel_size) const;
+	cv::Mat
+	ZeroOutGroundBFS(const cv::Mat &image, const cv::Mat &angle_image, const Radians &threshold,
+			int kernel_size) const;
 
-  /**
-   * @brief      create a help image with angle in radians written for each
-   *             pixel
-   *
-   * @param      depth_image  [input depth image]
-   * @return     [32 bit float image with angle in radians written in every
-   *             pixel]
-   */
-  cv::Mat CreateAngleImage(const cv::Mat& depth_image);
+	/**
+	 * @brief      create a help image with angle in radians written for each
+	 *             pixel
+	 *
+	 * @param      depth_image  [input depth image]
+	 * @return     [32 bit float image with angle in radians written in every
+	 *             pixel]
+	 */
+	cv::Mat
+	CreateAngleImage(const cv::Mat &depth_image);
 
-  /**
-   * @brief      Get kernel for Savitsky-Golay filter
-   * @details    Get a column filter to process an image filled with data with
-   *             Savitsky-Golay filter
-   *
-   * @param      window_size  size of the kernel
-   * @return     column Mat kernel
-   */
-  cv::Mat GetSavitskyGolayKernel(int window_size) const;
-  cv::Mat GetUniformKernel(int window_size, int type = CV_32F) const;
+	/**
+	 * @brief      Get kernel for Savitsky-Golay filter
+	 * @details    Get a column filter to process an image filled with data with
+	 *             Savitsky-Golay filter
+	 *
+	 * @param      window_size  size of the kernel
+	 * @return     column Mat kernel
+	 */
+	cv::Mat
+	GetSavitskyGolayKernel(int window_size) const;
+	cv::Mat
+	GetUniformKernel(int window_size, int type = CV_32F) const;
 
-  /**
-   * @brief      Apply Savitsky-Golay smoothing to a column
-   * @param      column  [A column of an angle image]
-   * @return     [a smoothed column]
-   */
+	/**
+	 * @brief      Apply Savitsky-Golay smoothing to a column
+	 * @param      column  [A column of an angle image]
+	 * @return     [a smoothed column]
+	 */
 
-  cv::Mat ApplySavitskyGolaySmoothing(const cv::Mat& column, int window_size);
-  /**
-   * @brief      Get line angle
-   * @details    Given two depth values and their angles compute the angle of
-   *             the line that they spawn in the sensor frame.
-   *
-   * @param      depth_image  [32 bit float image]
-   * @param      col          [current column]
-   * @param      row_curr     [current row]
-   * @param      row_neigh    [neighbor row]
-   * @return     [angle of the line]
-   */
-  Radians GetLineAngle(const cv::Mat& depth_image, int col, int row_curr,
-                       int row_neigh);
+	cv::Mat
+	ApplySavitskyGolaySmoothing(const cv::Mat &column, int window_size);
+	/**
+	 * @brief      Get line angle
+	 * @details    Given two depth values and their angles compute the angle of
+	 *             the line that they spawn in the sensor frame.
+	 *
+	 * @param      depth_image  [32 bit float image]
+	 * @param      col          [current column]
+	 * @param      row_curr     [current row]
+	 * @param      row_neigh    [neighbor row]
+	 * @return     [angle of the line]
+	 */
+	Radians
+	GetLineAngle(const cv::Mat &depth_image, int col, int row_curr, int row_neigh);
 
-  /**
-   * @brief      Repair zeros in the depth image
-   *
-   * @param[in]  depth_image  The depth image
-   *
-   * @return     depth image with repaired values
-   */
-  cv::Mat RepairDepth(const cv::Mat& no_ground_image, int step,
-                      float depth_threshold);
+	/**
+	 * @brief      Repair zeros in the depth image
+	 *
+	 * @param[in]  depth_image  The depth image
+	 *
+	 * @return     depth image with repaired values
+	 */
+	cv::Mat
+	RepairDepth(const cv::Mat &no_ground_image, int step, float depth_threshold);
 
-  cv::Mat RepairDepth(const cv::Mat& depth_image);
+	cv::Mat
+	RepairDepth(const cv::Mat &depth_image);
 
-  ProjectionParams _params;
-  int _window_size = 5;
-  Radians _ground_remove_angle = 5_deg;
-  float _eps = 0.001f;
+	ProjectionParams _params;
+	int _window_size = 5;
+	Radians _ground_remove_angle = 5_deg;
+	float _eps = 0.001f;
 
-  mutable int _counter = 0;
+	mutable int _counter = 0;
 };
 
 }  // namespace depth_clustering
