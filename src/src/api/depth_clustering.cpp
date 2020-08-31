ad1e91571c57fa9f6c291ce8545866709b2fead5
Simon Yu <jundayu2@illinois.edu>
2020-08-31 18:47:39 -0500

    Updated ground removal parameters.

--- src/src/api/depth_clustering.cpp
+++ src/src/api/depth_clustering.cpp
@@ -26,9 +26,9 @@ DepthClustering::DepthClustering()
 	outline_type_ = ObjectPainter::OutlineType::kBox;
 	size_cluster_min_ = 10;
 	size_cluster_max_ = 10000;
-	size_smooth_window_ = 5;
+	size_smooth_window_ = 7;
 	angle_clustering_ = 10_deg;
-	angle_ground_removal_ = 9_deg;
+	angle_ground_removal_ = 6_deg;
 	log_apollo_ = false;
 	log_data_ = true;
 }
