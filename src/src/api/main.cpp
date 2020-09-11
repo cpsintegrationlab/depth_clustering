001d2ecf0d5b6c8361ed494e5570b117f0595113
Simon Yu <jundayu2@illinois.edu>
2020-09-10 20:09:26 -0500

    Removed parameter handling from main process.

--- src/src/api/main.cpp
+++ src/src/api/main.cpp
@@ -13,14 +13,12 @@ main(int argc, char* argv[])
 	std::string dataset_path =
 			"../../../data/segment-1022527355599519580_4866_960_4886_960_with_camera_labels/";
 	const std::string dataset_file_type = ".tiff";
-	std::string bounding_box_type_string = "cube";
-	DepthClustering::Parameter depth_clustering_parameter;
 
 	if (argc > 1)
 	{
 		if (std::string(argv[1]) == "-h")
 		{
-			std::cout << std::endl << "Usage: " << argv[0] << " [dataset path] [bounding box type]"
+			std::cout << std::endl << "Usage: " << argv[0] << " [dataset path]"
 					<< std::endl << std::endl;
 			return 0;
 		}
@@ -28,21 +26,7 @@ main(int argc, char* argv[])
 		dataset_path = argv[1];
 	}
 
-	if (argc > 2)
-	{
-		bounding_box_type_string = argv[2];
-	}
-
-	if (bounding_box_type_string == "cube")
-	{
-		depth_clustering_parameter.bounding_box_type = BoundingBox::Type::Cube;
-	}
-	else if (bounding_box_type_string == "polygon")
-	{
-		depth_clustering_parameter.bounding_box_type = BoundingBox::Type::Polygon;
-	}
-
-	DepthClustering depth_clustering(depth_clustering_parameter);
+	DepthClustering depth_clustering;
 
 	if (!depth_clustering.initializeForDataset(dataset_path, dataset_file_type))
 	{
