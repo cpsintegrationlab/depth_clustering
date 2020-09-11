a33e2175407fb9acf9df16ca8c9b8ac03968e68b
Simon Yu <jundayu2@illinois.edu>
2020-09-10 22:03:49 -0500

    Added comments.

--- src/src/post_processing/camera_projection.cpp
+++ src/src/post_processing/camera_projection.cpp
@@ -77,9 +77,9 @@ CameraProjection::projectBoundingBoxFrameCube()
 
 	auto bounding_box_frame_cube = *(bounding_box_->getFrameCube());
 
-	Eigen::Matrix<double, 3, 4> camera_to_image;
-	Eigen::Matrix<double, 4, 4> world_to_camera;
-	Eigen::Matrix<double, 4, 4> world_to_camera_axes;
+	Eigen::Matrix<double, 3, 4> camera_to_image; // Camera intrinsic matrix, A
+	Eigen::Matrix<double, 4, 4> world_to_camera; // Camera extrinsic matrix, [R|t]
+	Eigen::Matrix<double, 4, 4> world_to_camera_axes; // Camera extrinsic matrix axes transformation matrix
 
 	camera_to_image << parameter_.intrinsic[0], 0, parameter_.intrinsic[2], 0, 0, parameter_.intrinsic[1], parameter_.intrinsic[3], 0, 0, 0, 1, 0;
 
@@ -90,9 +90,10 @@ CameraProjection::projectBoundingBoxFrameCube()
 
 	world_to_camera_axes << 0, -1, 0, 0, 0, 0, -1, 0, 1, 0, 0, 0, 0, 0, 0, 1;
 
-
+	// Inverse given camera-to-world extrinsic and transformation axes into camera frame
 	world_to_camera = world_to_camera_axes * world_to_camera.inverse();
 
+	// Remove extrinsic translation vector sicen bounding boxes are already in camera frame
 	world_to_camera(0, 3) = 0;
 	world_to_camera(1, 3) = 0;
 	world_to_camera(2, 3) = 0;
