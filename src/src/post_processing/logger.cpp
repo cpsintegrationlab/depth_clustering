ae3680d14131846bf301972f9067dcc509d3fa40
Simon Yu <jundayu2@illinois.edu>
2020-09-11 17:49:24 -0500

    Renaming.

--- src/src/post_processing/logger.cpp
+++ src/src/post_processing/logger.cpp
@@ -79,50 +79,51 @@ Logger::logBoundingBoxFrameCube(const std::string& frame_name)
 		return;
 	}
 
-	for (const auto &cube : *bounding_box_frame_cube)
+	for (const auto &bounding_box_cube : *bounding_box_frame_cube)
 	{
-		boost::property_tree::ptree cube_array_value;
-		boost::property_tree::ptree cube_array;
+		boost::property_tree::ptree bounding_box_cube_array_value;
+		boost::property_tree::ptree bounding_box_cube_array;
 
-		auto center = cube.first;
-		auto extent = cube.second;
+		auto center = bounding_box_cube.first;
+		auto extent = bounding_box_cube.second;
 
-		cube_array_value.put_value(center.x());
-		cube_array.push_back(std::make_pair("", cube_array_value));
+		bounding_box_cube_array_value.put_value(center.x());
+		bounding_box_cube_array.push_back(std::make_pair("", bounding_box_cube_array_value));
 
-		cube_array_value.put_value(center.y());
-		cube_array.push_back(std::make_pair("", cube_array_value));
+		bounding_box_cube_array_value.put_value(center.y());
+		bounding_box_cube_array.push_back(std::make_pair("", bounding_box_cube_array_value));
 
-		cube_array_value.put_value(center.z());
-		cube_array.push_back(std::make_pair("", cube_array_value));
+		bounding_box_cube_array_value.put_value(center.z());
+		bounding_box_cube_array.push_back(std::make_pair("", bounding_box_cube_array_value));
 
-		cube_array_value.put_value(extent.x());
-		cube_array.push_back(std::make_pair("", cube_array_value));
+		bounding_box_cube_array_value.put_value(extent.x());
+		bounding_box_cube_array.push_back(std::make_pair("", bounding_box_cube_array_value));
 
-		cube_array_value.put_value(extent.y());
-		cube_array.push_back(std::make_pair("", cube_array_value));
+		bounding_box_cube_array_value.put_value(extent.y());
+		bounding_box_cube_array.push_back(std::make_pair("", bounding_box_cube_array_value));
 
-		cube_array_value.put_value(extent.z());
-		cube_array.push_back(std::make_pair("", cube_array_value));
+		bounding_box_cube_array_value.put_value(extent.z());
+		bounding_box_cube_array.push_back(std::make_pair("", bounding_box_cube_array_value));
 
-		auto cloud_file_array_optional = bounding_box_log_tree_cube_.get_child_optional(
-				boost::property_tree::ptree::path_type(frame_name, '/'));
+		auto bounding_box_cube_frame_array_optional =
+				bounding_box_log_tree_cube_.get_child_optional(
+						boost::property_tree::ptree::path_type(frame_name, '/'));
 
-		if (!cloud_file_array_optional)
+		if (!bounding_box_cube_frame_array_optional)
 		{
 			bounding_box_log_tree_cube_.add_child(
 					boost::property_tree::ptree::path_type(frame_name, '/'),
 					boost::property_tree::ptree());
-			auto &cloud_file_array = bounding_box_log_tree_cube_.get_child(
+			auto &bounding_box_cube_frame_array = bounding_box_log_tree_cube_.get_child(
 					boost::property_tree::ptree::path_type(frame_name, '/'));
 
-			cloud_file_array.push_back(std::make_pair("", cube_array));
+			bounding_box_cube_frame_array.push_back(std::make_pair("", bounding_box_cube_array));
 		}
 		else
 		{
-			auto &cloud_file_array = *cloud_file_array_optional;
+			auto &bounding_box_cube_frame_array = *bounding_box_cube_frame_array_optional;
 
-			cloud_file_array.push_back(std::make_pair("", cube_array));
+			bounding_box_cube_frame_array.push_back(std::make_pair("", bounding_box_cube_array));
 		}
 	}
 }
@@ -149,56 +150,63 @@ Logger::logBoundingBoxFramePolygon(const std::string& frame_name)
 		return;
 	}
 
-	for (const auto &polygon : *bounding_box_frame_polygon)
+	for (const auto &bounding_box_polygon : *bounding_box_frame_polygon)
 	{
-		boost::property_tree::ptree cloud_object_array_value;
-		boost::property_tree::ptree cloud_object_array;
-		boost::property_tree::ptree cloud_hull_vector_array_value;
-		boost::property_tree::ptree cloud_hull_vector_array;
-		boost::property_tree::ptree cloud_hull_array;
+		boost::property_tree::ptree bounding_box_polygon_array_value;
+		boost::property_tree::ptree bounding_box_polygon_array;
+		boost::property_tree::ptree bounding_box_polygon_hull_vector_array_value;
+		boost::property_tree::ptree bounding_box_polygon_hull_vector_array;
+		boost::property_tree::ptree bounding_box_polygon_hull_array;
 
-		auto hull = polygon.first;
-		auto diff_z = polygon.second;
+		auto hull = bounding_box_polygon.first;
+		auto diff_z = bounding_box_polygon.second;
 
 		for (const auto &hull_vector : hull)
 		{
-			cloud_hull_vector_array_value.put_value(hull_vector.x());
-			cloud_hull_vector_array.push_back(std::make_pair("", cloud_hull_vector_array_value));
+			bounding_box_polygon_hull_vector_array_value.put_value(hull_vector.x());
+			bounding_box_polygon_hull_vector_array.push_back(
+					std::make_pair("", bounding_box_polygon_hull_vector_array_value));
 
-			cloud_hull_vector_array_value.put_value(hull_vector.y());
-			cloud_hull_vector_array.push_back(std::make_pair("", cloud_hull_vector_array_value));
+			bounding_box_polygon_hull_vector_array_value.put_value(hull_vector.y());
+			bounding_box_polygon_hull_vector_array.push_back(
+					std::make_pair("", bounding_box_polygon_hull_vector_array_value));
 
-			cloud_hull_vector_array_value.put_value(hull_vector.z());
-			cloud_hull_vector_array.push_back(std::make_pair("", cloud_hull_vector_array_value));
+			bounding_box_polygon_hull_vector_array_value.put_value(hull_vector.z());
+			bounding_box_polygon_hull_vector_array.push_back(
+					std::make_pair("", bounding_box_polygon_hull_vector_array_value));
 
-			cloud_hull_array.push_back(std::make_pair("", cloud_hull_vector_array));
+			bounding_box_polygon_hull_array.push_back(
+					std::make_pair("", bounding_box_polygon_hull_vector_array));
 
-			cloud_hull_vector_array.clear();
+			bounding_box_polygon_hull_vector_array.clear();
 		}
 
-		cloud_object_array.push_back(std::make_pair("", cloud_hull_array));
+		bounding_box_polygon_array.push_back(std::make_pair("", bounding_box_polygon_hull_array));
 
-		cloud_object_array_value.put_value(diff_z);
-		cloud_object_array.push_back(std::make_pair("", cloud_object_array_value));
+		bounding_box_polygon_array_value.put_value(diff_z);
+		bounding_box_polygon_array.push_back(std::make_pair("", bounding_box_polygon_array_value));
 
-		auto cloud_file_array_optional = bounding_box_log_tree_polygon_.get_child_optional(
-				boost::property_tree::ptree::path_type(frame_name, '/'));
+		auto bounding_box_polygon_frame_array_optional =
+				bounding_box_log_tree_polygon_.get_child_optional(
+						boost::property_tree::ptree::path_type(frame_name, '/'));
 
-		if (!cloud_file_array_optional)
+		if (!bounding_box_polygon_frame_array_optional)
 		{
 			bounding_box_log_tree_polygon_.add_child(
 					boost::property_tree::ptree::path_type(frame_name, '/'),
 					boost::property_tree::ptree());
-			auto &cloud_file_array = bounding_box_log_tree_polygon_.get_child(
+			auto &bounding_box_polygon_frame_array = bounding_box_log_tree_polygon_.get_child(
 					boost::property_tree::ptree::path_type(frame_name, '/'));
 
-			cloud_file_array.push_back(std::make_pair("", cloud_object_array));
+			bounding_box_polygon_frame_array.push_back(
+					std::make_pair("", bounding_box_polygon_array));
 		}
 		else
 		{
-			auto &cloud_file_array = *cloud_file_array_optional;
+			auto &bounding_box_polygon_frame_array = *bounding_box_polygon_frame_array_optional;
 
-			cloud_file_array.push_back(std::make_pair("", cloud_object_array));
+			bounding_box_polygon_frame_array.push_back(
+					std::make_pair("", bounding_box_polygon_array));
 		}
 	}
 }
@@ -225,48 +233,49 @@ Logger::logBoundingBoxFrameFlat(const std::string& frame_name)
 		return;
 	}
 
-	for (const auto &flat : *bounding_box_frame_flat)
+	for (const auto &bounding_box_flat : *bounding_box_frame_flat)
 	{
-		boost::property_tree::ptree flat_array_value;
-		boost::property_tree::ptree flat_array;
+		boost::property_tree::ptree bounding_box_flat_array_value;
+		boost::property_tree::ptree bounding_box_flat_array;
 
-		auto corner_upper_left = std::get<0>(flat);
-		auto corner_lower_right = std::get<1>(flat);
-		auto depth = std::get<2>(flat);
+		auto corner_upper_left = std::get<0>(bounding_box_flat);
+		auto corner_lower_right = std::get<1>(bounding_box_flat);
+		auto depth = std::get<2>(bounding_box_flat);
 
-		flat_array_value.put_value(corner_upper_left.x());
-		flat_array.push_back(std::make_pair("", flat_array_value));
+		bounding_box_flat_array_value.put_value(corner_upper_left.x());
+		bounding_box_flat_array.push_back(std::make_pair("", bounding_box_flat_array_value));
 
-		flat_array_value.put_value(corner_upper_left.y());
-		flat_array.push_back(std::make_pair("", flat_array_value));
+		bounding_box_flat_array_value.put_value(corner_upper_left.y());
+		bounding_box_flat_array.push_back(std::make_pair("", bounding_box_flat_array_value));
 
-		flat_array_value.put_value(corner_lower_right.x());
-		flat_array.push_back(std::make_pair("", flat_array_value));
+		bounding_box_flat_array_value.put_value(corner_lower_right.x());
+		bounding_box_flat_array.push_back(std::make_pair("", bounding_box_flat_array_value));
 
-		flat_array_value.put_value(corner_lower_right.y());
-		flat_array.push_back(std::make_pair("", flat_array_value));
+		bounding_box_flat_array_value.put_value(corner_lower_right.y());
+		bounding_box_flat_array.push_back(std::make_pair("", bounding_box_flat_array_value));
 
-		flat_array_value.put_value(depth);
-		flat_array.push_back(std::make_pair("", flat_array_value));
+		bounding_box_flat_array_value.put_value(depth);
+		bounding_box_flat_array.push_back(std::make_pair("", bounding_box_flat_array_value));
 
-		auto cloud_file_array_optional = bounding_box_log_tree_flat_.get_child_optional(
-				boost::property_tree::ptree::path_type(frame_name, '/'));
+		auto bounding_box_flat_frame_array_optional =
+				bounding_box_log_tree_flat_.get_child_optional(
+						boost::property_tree::ptree::path_type(frame_name, '/'));
 
-		if (!cloud_file_array_optional)
+		if (!bounding_box_flat_frame_array_optional)
 		{
 			bounding_box_log_tree_flat_.add_child(
 					boost::property_tree::ptree::path_type(frame_name, '/'),
 					boost::property_tree::ptree());
-			auto &cloud_file_array = bounding_box_log_tree_flat_.get_child(
+			auto &bounding_box_flat_frame_array = bounding_box_log_tree_flat_.get_child(
 					boost::property_tree::ptree::path_type(frame_name, '/'));
 
-			cloud_file_array.push_back(std::make_pair("", flat_array));
+			bounding_box_flat_frame_array.push_back(std::make_pair("", bounding_box_flat_array));
 		}
 		else
 		{
-			auto &cloud_file_array = *cloud_file_array_optional;
+			auto &bounding_box_flat_frame_array = *bounding_box_flat_frame_array_optional;
 
-			cloud_file_array.push_back(std::make_pair("", flat_array));
+			bounding_box_flat_frame_array.push_back(std::make_pair("", bounding_box_flat_array));
 		}
 	}
 }
