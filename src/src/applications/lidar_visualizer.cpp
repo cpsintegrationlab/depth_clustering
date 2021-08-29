68f439ae436637a52840f030615da0c99d52f6c4
Simon Yu <jundayu2@illinois.edu>
2021-08-29 15:07:03 -0500

    Manually merge gui branch visualization

---
+++ src/src/applications/lidar_visualizer.cpp
@@ -0,0 +1,33 @@
+#include <QApplication>
+#include <QMainWindow>
+
+#include "visualization/widgets/opengl_folder_player.h"
+
+int
+main(int argc, char* argv[])
+{
+	bool debug = false;
+
+	if (argc > 1)
+	{
+		std::string mode = argv[1];
+
+		if (mode == "debug")
+		{
+			debug = true;
+		}
+	}
+
+	if (!debug)
+	{
+		__attribute__((unused)) FILE *file;
+		file = freopen("/dev/null", "w", stderr);
+	}
+
+	QApplication a(argc, argv);
+
+	OpenGlFolderPlayer folder_player_widget;
+	folder_player_widget.show();
+
+	return a.exec();
+}
