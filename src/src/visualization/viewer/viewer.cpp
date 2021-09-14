bac3797a27f701addfcd03b781d992b0b06d570b
Simon Yu <jundayu2@illinois.edu>
2021-09-13 19:27:28 -0500

    Update point cloud viewer initial orientation

--- src/src/visualization/viewer/viewer.cpp
+++ src/src/visualization/viewer/viewer.cpp
@@ -2,31 +2,44 @@
 // In case of any problems with the code please contact me.
 // Email: igor.bogoslavskyi@uni-bonn.de.
 
-#include "./viewer.h"
+#include <quaternion.h>
 
+#include "visualization/viewer/viewer.h"
+
+using qglviewer::Quaternion;
 using std::lock_guard;
 using std::mutex;
 
-void Viewer::AddDrawable(Drawable::Ptr drawable) {
-  lock_guard<mutex> guard(_cloud_mutex);
-  _drawables.push_back(drawable);
+void
+Viewer::AddDrawable(Drawable::Ptr drawable)
+{
+	lock_guard<mutex> guard(_cloud_mutex);
+	_drawables.push_back(drawable);
 }
 
-void Viewer::Clear() {
-  lock_guard<mutex> guard(_cloud_mutex);
-  _drawables.clear();
+void
+Viewer::Clear()
+{
+	lock_guard<mutex> guard(_cloud_mutex);
+	_drawables.clear();
 }
 
-void Viewer::draw() {
-  lock_guard<mutex> guard(_cloud_mutex);
-  for (auto& drawable : _drawables) {
-    drawable->Draw();
-  }
+void
+Viewer::draw()
+{
+	lock_guard<mutex> guard(_cloud_mutex);
+	for (auto &drawable : _drawables)
+	{
+		drawable->Draw();
+	}
 }
 
-void Viewer::init() {
-  setSceneRadius(75.0);
-  setBackgroundColor(QColor(0, 0, 0));
-  camera()->showEntireScene();
-  glDisable(GL_LIGHTING);
+void
+Viewer::init()
+{
+	setSceneRadius(75.0);
+	setBackgroundColor(QColor(0, 0, 0));
+	camera()->setOrientation(Quaternion(0, 0, -0.7071068, 0.7071068));
+	camera()->showEntireScene();
+	glDisable(GL_LIGHTING);
 }
--- src/src/visualization/viewer/viewer.h
+++ src/src/visualization/viewer/viewer.h
@@ -5,27 +5,44 @@
 #ifndef SRC_QT_VIEWER_VIEWER_H_
 #define SRC_QT_VIEWER_VIEWER_H_
 
-#include <QGLViewer/qglviewer.h>
-
 #include <mutex>
+#include <QGLViewer/qglviewer.h>
 #include <vector>
 
 #include "visualization/drawables/drawable.h"
 
-class Viewer : public QGLViewer {
- public:
-  explicit Viewer(QWidget* parent = 0) : QGLViewer(parent) {}
-  void AddDrawable(Drawable::Ptr drawable);
-  void Clear();
-  ~Viewer() override {}
+class Viewer: public QGLViewer
+{
+public:
 
- protected:
-  void draw() override;
-  void init() override;
+	explicit
+	Viewer(QWidget* parent = 0) :
+			QGLViewer(parent)
+	{
+	}
 
- private:
-  std::vector<Drawable::Ptr> _drawables;
-  mutable std::mutex _cloud_mutex;
+	void
+	AddDrawable(Drawable::Ptr drawable);
+
+	void
+	Clear();
+
+	~Viewer() override
+	{
+	}
+
+protected:
+
+	void
+	draw() override;
+
+	void
+	init() override;
+
+private:
+
+	std::vector<Drawable::Ptr> _drawables;
+	mutable std::mutex _cloud_mutex;
 };
 
 #endif  // SRC_QT_VIEWER_VIEWER_H_
